cc716e23df27c4d5154f2c2f54893495e5aec71c
adam-formela <59099128+adam-formela@users.noreply.github.com>
2022-02-27 17:39:29 +0100

    Delete CodeMe.py

--- CodeMe.py
+++
@@ -1,8 +0,0 @@
-# BMI = (masa (kg)) / (wzrost (m))2
-
-waga = input('Podaj swoja wage: ')
-wzrost = input('Podaj swoj wzrost w m: ')
-
-BMI = (float(waga) / float(wzrost)**2)
-print(f'Twoje BMI wynosi: {BMI}')
-
